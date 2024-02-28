7e86fdc0ec50be57d98424bb0ca385a117a1e3e8
WillhKessler <willhkessler+github@gmail.com>
2024-02-28 08:19:42 -0500

    updates to extract_nested_zip()

--- unzip_all.py
+++ unzip_all.py
@@ -1,6 +1,7 @@
 import zipfile, re, os, os.path, glob
 
-vars = ["ppt", "tdmean", "tmax", "tmin", "tmean", "vpdmax", "vpdmin"]
+vars = ["tdmean", "tmax", "tmin", "tmean", "vpdmax", "vpdmin"]
+
 destination = "S:\\GCMC\\Data\\Climate\\PRISM"
 
 
@@ -11,16 +12,25 @@ def extract_nested_zip(zippedFile, toFolder):
     with zipfile.ZipFile(zippedFile, "r") as zfile:
         zfile.extractall(path=toFolder)
     os.remove(zippedFile)
-    for root, dirs, files in os.walk(toFolder):
-        for filename in files:
-            if re.search(r"\.zip$", filename):
-                fileSpec = os.path.join(root, filename)
-                extract_nested_zip(fileSpec, root)
+    # for root, dirs, files in os.walk(toFolder):
+    #     print(root)
+    #     for filename in files:
+    #         print(files)
+    # if re.search(r"\.zip$", filename):
+    #     fileSpec = os.path.join(root, filename)
+    #     extract_nested_zip(fileSpec, root)
 
 
 for i in vars:
-    files = glob.glob(os.path.join(destination, i, "daily_4km", "*.zip"))
+    print(i)
+    files = glob.glob(os.path.join(destination, i, "daily_4km", "*"))
+    # files = glob.glob(os.path.join(destination, i, "daily_4km", "*.zip"))
+    # print(files)
     for ii in files:
-        extract_nested_zip(
-            zippedFile=ii, toFolder=os.path.join(destination, i, "daily_4km")
-        )
+        print(ii)
+        zips = glob.glob(os.path.join(ii, "*.zip"))
+        # print(os.path.join(destination, i, "daily_4km", "*"))
+        # extract_nested_zip(zippedFile=ii, toFolder=os.path.join(ii))
+        for iii in zips:
+            print(iii)
+            extract_nested_zip(iii, os.path.join(ii))
